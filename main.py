cb9ac877a6b0383fefc4f74c91259cca5380092e
adeptcustard <115986323+adeptcustard@users.noreply.github.com>
2026-03-30 19:52:57 +0100

    add player, circleshape classes and update existing files

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pygame
 from logger import log_state
 import constants
+from player import Player
 
 def main():
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
@@ -9,7 +10,12 @@ def main():
 
     pygame.init()
 
-    screen = pygame.display.set_mode((constants.SCREEN_WIDTH, constants.SCREEN_HEIGHT))
+    game_screen = pygame.display.set_mode((constants.SCREEN_WIDTH, constants.SCREEN_HEIGHT))
+    game_clock = pygame.time.Clock()
+
+    dt = 0
+
+    player = Player((constants.SCREEN_WIDTH / 2),(constants.SCREEN_HEIGHT / 2))
 
     while True:
         log_state()
@@ -18,8 +24,13 @@ def main():
             if event.type == pygame.QUIT:
                 return
         
-        screen.fill("black")
+        dt = game_clock.tick(60) / 1000
+
+        game_screen.fill("black")
+        player.draw(game_screen)
+
         pygame.display.flip()
+
 
 if __name__ == "__main__":
     main()
